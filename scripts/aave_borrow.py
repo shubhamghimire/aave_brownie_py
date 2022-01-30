a2b77bd7c629a70eead17b19d8adc5c79a185257
shubhamghimire <shubhamghimire1996@gmail.com>
2022-01-30 21:39:36 +0900

    lending pool adress provioder added

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -8,3 +8,12 @@ def main():
     erc20_address = config["networks"][network.show_active()]["weth_token"]
     if network.show_active() in ["mainnet-fork"]:
         get_weth()
+    # ABI
+    # Address
+    lending_pool = get_lending_pool()
+
+
+def get_lending_pool():
+    # ABI
+    # Address
+    pass
