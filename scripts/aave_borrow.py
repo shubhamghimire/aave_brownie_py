d36e034ea0d3771b85583742bdd61fbe4e837db9
shubhamghimire <shubhamghimire1996@gmail.com>
2022-02-02 21:13:42 +0900

    get_borrowable_data function created and tested

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -22,6 +22,26 @@ def main():
     tx.wait(1)
     print("Deposited!")
 
+    borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_threshold,
+        ltv,
+        health_factor,
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+    print(f"You have {total_collateral_eth} worth of ETH deposited.")
+    print(f"You have {total_debt_eth} worth of ETH borrowed.")
+    print(f"You have {available_borrow_eth} worth of ETH.")
+    return (float(available_borrow_eth), float(total_debt_eth))
+
 
 def approve_erc20(amount, spender, erc20_address, account):
     print("Approving ERC20 token...")
